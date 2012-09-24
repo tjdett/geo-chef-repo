a65bb44ecd60447f5c0bdc4685e2101dd749ce8c
Tim Dettrick <tim@dettrick.net>
2012-09-24 16:40:02 +1000

    Extracting solr

--- cookbooks/geo/recipes/default.rb
+++ cookbooks/geo/recipes/default.rb
@@ -9,4 +9,19 @@
 
 mypkgs = ["java-1.7.0-openjdk", "subversion"]
 
-mypkgs.each {|p| package p }
+mypkgs.each do |p|
+  package p
+end
+
+script "extract_solr" do
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+    cd /opt
+    wget http://apache.mirror.uber.com.au/lucene/solr/3.6.0/apache-solr-3.6.0.tgz
+    tar xzvf apache-solr-3.6.0.tgz
+  EOH
+  not_if do
+    File.exists?("/opt/apache-solr-3.6.0")
+  end
+end 
